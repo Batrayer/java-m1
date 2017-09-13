1dd75248c30ce8e547b39539ca291349606b1e2a
Baptiste <batrayer77@gmail.com>
2017-09-13 11:46:12 +0200

    Ajout de rectangle & Square

---
+++ td1/src/fr/parisnanterre/miage/poa/td1/shapes/Square.java
@@ -0,0 +1,7 @@
+package fr.parisnanterre.miage.poa.td1.shapes;
+
+public class Square extends Rectangle {
+    public Square(Point2D refpoint2, double length) {
+        super(refpoint2, length, length);
+    }
+}
