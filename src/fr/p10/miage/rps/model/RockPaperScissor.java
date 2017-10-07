a883f45c7094a0ee2a7b0b75b769feef7d5961f6
Baptiste <batrayer77@gmail.com>
2017-10-07 12:13:13 +0200

    Created : function for RPS and added test for tie and win

--- src/fr/p10/miage/rps/model/RockPaperScissor.java
+++ src/fr/p10/miage/rps/model/RockPaperScissor.java
@@ -3,6 +3,37 @@ package fr.p10.miage.rps.model;
 public class RockPaperScissor {
 
     public Result Result_Play(RPSEnum p1, RPSEnum p2){
-        return Result.LOST;
+        if(p1.equals(RPSEnum.ROCK) && p2.equals(RPSEnum.PAPER)){
+            return Result.LOST;
+        }
+        if(p1.equals(RPSEnum.ROCK) && p2.equals(RPSEnum.SCISSOR)){
+            return Result.WIN;
+        }
+        if(p1.equals(RPSEnum.ROCK)&&p2.equals(RPSEnum.ROCK))
+        {
+            return Result.TIE;
+        }
+        if(p1.equals(RPSEnum.PAPER) && p2.equals(RPSEnum.ROCK))
+        {
+            return Result.WIN;
+        }
+        if(p1.equals(RPSEnum.PAPER)&&p2.equals(RPSEnum.SCISSOR)){
+            return Result.LOST;
+        }
+        if(p1.equals(RPSEnum.PAPER) && p2.equals(RPSEnum.PAPER)){
+            return Result.TIE;
+        }
+        if(p1.equals(RPSEnum.SCISSOR) && p2.equals(RPSEnum.SCISSOR))
+        {
+            return Result.TIE;
+        }
+        if(p1.equals(RPSEnum.SCISSOR) && p2.equals(RPSEnum.PAPER))
+        {
+            return Result.WIN;
+        }
+        if(p1.equals(RPSEnum.SCISSOR) && p2.equals(RPSEnum.ROCK)){
+            return Result.LOST;
+        }
+        return null;
     }
 }
--- tests/fr/p10/miage/rps/tests/RockPaperScissorTest.java
+++ tests/fr/p10/miage/rps/tests/RockPaperScissorTest.java
@@ -1,7 +1,34 @@
 package fr.p10.miage.rps.tests;
 
+import fr.p10.miage.rps.model.RPSEnum;
+import fr.p10.miage.rps.model.Result;
+import fr.p10.miage.rps.model.RockPaperScissor;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class RockPaperScissorTest {
+    private RockPaperScissor rps;
 
+    @BeforeMethod
+    public void setUp() throws Exception {
+        this.rps = new RockPaperScissor();
+    }
+    @Parameters({"rock","scissor"})
+    @Test
+    public void testWinPlay(String p1, String p2){
+        assertEquals(rps.Result_Play(RPSEnum.valueOf(p1),RPSEnum.valueOf(p2)), Result.WIN);
+    }
+    @Parameters({"rock", "rock"})
+    @Test
+    public void testTiePlay(String p1, String p2){
+        assertEquals(rps.Result_Play(RPSEnum.valueOf(p1),RPSEnum.valueOf(p2)), Result.TIE);
+    }
+    @AfterMethod
+    public void tearDown() throws Exception {
+        this.rps = null;
+    }
 }
